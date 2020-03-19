beb8d64784b3619aa6c3c9e0f499d9c454d0b861
Andy Jones <andytjones@icloud.com>
2020-03-19 19:36:08 +0000

    Code - added resources to be provisioned section.

--- hello-world.tf
+++ hello-world.tf
@@ -45,3 +45,58 @@ data "aws_ami" "aws-linux" {
     values = ["hvm"]
   }
 }
+
+####
+# RESOURCES
+####
+
+resource "aws_default_vpc" "default" {
+
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name = "nginx_demo"
+  description = "Allow ports for nginx demo"
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port =  0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_instance" "nginx" {
+  ami = data.aws_ami.aws-linux.id
+  instance_type = "t2.micro"
+  key_name = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  connection {
+    type = "ssh"
+    host = self.public_ip
+    user = "ec2-user"
+    private_key = file(var.private_key_path)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install nginx -y",
+      "sudo service nginx start"
+    ]
+  }
+}
