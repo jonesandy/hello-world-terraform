838fa005c14987587b90659a2886faac0463640b
Andy Jones <andytjones@icloud.com>
2020-03-19 19:15:09 +0000

    Code - Added provider in file.

--- hello-world.tf
+++ hello-world.tf
@@ -11,3 +11,13 @@ variable "key_name" {}
 variable "region" {
   default = "us-east-1"
 }
+
+####
+# PROVIDERS
+####
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region = var.region
+}
