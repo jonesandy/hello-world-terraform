0585c4b7eb6db027bb708114666646f8bd8fd39c
Andy Jones <andytjones@icloud.com>
2020-03-21 18:58:07 +0000

    fix - corrected spelling mistake.

--- hello-world.tf
+++ hello-world.tf
@@ -118,7 +118,7 @@ resource "aws_security_group" "elb-sg" {
     from_port  = 80
     to_port    = 80
     protocol   = "tcp"
-    cidr_block = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
