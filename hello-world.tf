9932eab6bc9512b3d3491578ec7ca8af94419949
Andy Jones <andytjones@icloud.com>
2020-03-21 16:33:37 +0000

    feature - updated variables to hold subnet and workspace

--- hello-world.tf
+++ hello-world.tf
@@ -12,6 +12,13 @@ variable "region" {
   default = "us-east-1"
 }
 
+variable "network_address_space" {
+  default = "10.1.0.0/16"
+}
+variable "subnet1_address_space" {
+  default = "10.1.0.0/24"
+}
+
 ####
 # PROVIDERS
 ####
